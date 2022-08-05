76ddf703afaa998ce176a19bcbe66a5e200eb685
Yoon321 <tkdwls734@gmail.com>
2022-08-05 14:54:28 +0900

    자바 알고리즘 연습 60제 1번

---
+++ Problem10869.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Problem10869 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int firstNum = scanner.nextInt();
+        int secondNum = scanner.nextInt();
+
+        System.out.println(firstNum + secondNum);
+        System.out.println(firstNum - secondNum);
+        System.out.println(firstNum * secondNum);
+        System.out.println(firstNum / secondNum);
+        System.out.println(firstNum % secondNum);
+    }
+}
